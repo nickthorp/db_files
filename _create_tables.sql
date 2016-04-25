8df9287992ace9276ab6d21be4ba4c63b9f85bb4
nickthorp <nickthorp20@gmail.com>
2016-04-24 19:25:10 -0500

    Syntax fixes

--- _create_tables.sql
+++ _create_tables.sql
@@ -4,18 +4,22 @@ DROP TABLE IF EXISTS hoard_user;
 DROP TABLE IF EXISTS hoard_book;
 
 CREATE TABLE hoard_user(
-  user_email VARCHAR(40) NOT NULL PRIMARY KEY,
+  user_email VARCHAR(40) NOT NULL,
   user_username VARCHAR(20) NOT NULL,
   user_firstname VARCHAR(15),
-  user_lastname VARCHAR(20) );
+  user_lastname VARCHAR(20),
+  PRIMARY KEY ( user_email ) );
 
 CREATE TABLE hoard_book(
-  book_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  book_email VARCHAR(40) NOT NULL FOREIGN KEY,
+  book_id INT NOT NULL AUTO_INCREMENT,
+  book_email VARCHAR(40) NOT NULL,
   book_title VARCHAR(128) NOT NULL,
   book_author VARCHAR(40),
   book_publisher VARCHAR(30),
   book_isbn VARCHAR(14),
   book_edition SMALLINT,
   book_isread BOOLEAN,
-  book_isreading BOOLEAN );
+  book_isreading BOOLEAN,
+  PRIMARY KEY ( book_id ),
+  FOREIGN KEY ( book_email ) REFERENCES hoard_user( user_email )
+ );
