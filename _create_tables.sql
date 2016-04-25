bcf4ebd95a3b5aee50a8d09035d70ad150ccbea1
nickthorp <nickthorp20@gmail.com>
2016-04-24 21:42:39 -0500

    Syntax fixes

--- _create_tables.sql
+++ _create_tables.sql
@@ -18,7 +18,7 @@ CREATE TABLE hoard_user(
 
 CREATE TABLE hoard_book(
   book_id INT NOT NULL AUTO_INCREMENT,
-  book_email VARCHAR(40) NOT NULL,
+  user_email VARCHAR(40) NOT NULL,
   book_title VARCHAR(128) NOT NULL,
   book_author VARCHAR(40),
   book_publisher VARCHAR(30),
@@ -27,12 +27,12 @@ CREATE TABLE hoard_book(
   book_is_read BOOLEAN,
   book_is_reading BOOLEAN,
   PRIMARY KEY ( book_id ),
-  FOREIGN KEY ( book_email ) REFERENCES hoard_user( user_email )
+  FOREIGN KEY ( user_email ) REFERENCES hoard_user( user_email )
  );
 
 CREATE TABLE hoard_comic(
   comic_id INT NOT NULL AUTO_INCREMENT,
-  comic_email VARCHAR(40) NOT NULL,
+  user_email VARCHAR(40) NOT NULL,
   comic_title VARCHAR(128) NOT NULL,
   comic_author VARCHAR(40),
   comic_artist VARCHAR(40),
@@ -41,44 +41,44 @@ CREATE TABLE hoard_comic(
   comic_is_read BOOLEAN,
   comic_is_reading BOOLEAN,
   PRIMARY KEY ( comic_id ),
-  FOREIGN KEY ( comic_email ) REFERENCES hoard_user( user_email )
+  FOREIGN KEY ( user_email ) REFERENCES hoard_user( user_email )
 );
 
 CREATE TABLE hoard_movie(
   movie_id INT NOT NULL AUTO_INCREMENT,
-  movie_email VARCHAR(40) NOT NULL,
+  user_email VARCHAR(40) NOT NULL,
   movie_title VARCHAR(128) NOT NULL,
   movie_release_year INT,
   movie_director VARCHAR(40),
   movie_is_watched BOOLEAN,
   movie_is_watching BOOLEAN,
   PRIMARY KEY ( movie_id ),
-  FOREIGN KEY ( movie_email ) REFERENCES hoard_user( user_email )
+  FOREIGN KEY ( user_email ) REFERENCES hoard_user( user_email )
 );
 
 CREATE TABLE hoard_music(
   music_id INT NOT NULL AUTO_INCREMENT,
-  music_email VARCHAR(40) NOT NULL,
+  user_email VARCHAR(40) NOT NULL,
   music_artist VARCHAR(40) NOT NULL,
   music_album_title VARCHAR(128) NOT NULL,
   music_is_listening BOOLEAN,
   PRIMARY KEY ( music_id ),
-  FOREIGN KEY ( music_email ) REFERENCES hoard_user( user_email )
+  FOREIGN KEY ( user_email ) REFERENCES hoard_user( user_email )
 );
 
 CREATE TABLE hoard_tabletop(
   tabletop_id INT NOT NULL AUTO_INCREMENT,
-  tabletop_email VARCHAR(40) NOT NULL,
+  user_email VARCHAR(40) NOT NULL,
   tabletop_title VARCHAR(128) NOT NULL,
   tabletop_min_player INT,
   tabletop_max_player INT,
   PRIMARY KEY ( tabletop_id ),
-  FOREIGN KEY ( tabletop_email ) REFERENCES hoard_user( user_email )
+  FOREIGN KEY ( user_email ) REFERENCES hoard_user( user_email )
 );
 
 CREATE TABLE hoard_tv_series(
   tv_series_id INT NOT NULL AUTO_INCREMENT,
-  tv_series_email VARCHAR(40) NOT NULL,
+  user_email VARCHAR(40) NOT NULL,
   tv_series_title VARCHAR(128) NOT NULL,
   tv_series_release_year INT,
   tv_series_director VARCHAR(40),
@@ -86,17 +86,17 @@ CREATE TABLE hoard_tv_series(
   tv_series_is_watched BOOLEAN,
   tv_series_is_watching BOOLEAN,
   PRIMARY KEY ( tv_series_id ),
-  FOREIGN KEY ( tv_series_email ) REFERENCES hoard_user( user_email )
+  FOREIGN KEY ( user_email ) REFERENCES hoard_user( user_email )
 );
 
 CREATE TABLE hoard_videogame(
   videogame_id INT NOT NULL AUTO_INCREMENT,
-  videogame_email VARCHAR(40) NOT NULL,
+  user_email VARCHAR(40) NOT NULL,
   videogame_title VARCHAR(128) NOT NULL,
   videogame_developer VARCHAR(30) NOT NULL,
   videogame_is_played BOOLEAN,
   videogame_is_playing BOOLEAN,
   videogame_is_complete BOOLEAN,
   PRIMARY KEY ( videogame_id ),
-  FOREIGN KEY ( videogame_email ) REFERENCES hoard_user( user_email )
+  FOREIGN KEY ( user_email ) REFERENCES hoard_user( user_email )
 );
